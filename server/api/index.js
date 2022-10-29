a7ea6dbe4140d01485874b70285e075f78521031
JackPadalino <pada0867@gmail.com>
2022-10-29 08:41:27 -0400

    commented out code for Year model - not using

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/year',require('./year'));
+//router.use('/year',require('./year'));
 router.use('/month', require('./month'));
 router.use('/day', require('./day'));
 router.use('/event', require('./event'));
